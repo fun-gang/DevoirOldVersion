4def5c1b6a57af95b3b6e048aa92a976dc8a0490
Marat <maratdiarov200619@gmail.com>
2024-01-20 16:53:31 +0500

    -Cleared-

--- Assets/Scripts/UI/PlayAnim.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayAnim : MonoBehaviour
-{
-    public string animName;
-    public void Play() => gameObject.GetComponent<Animator>().Play(animName);
-}
